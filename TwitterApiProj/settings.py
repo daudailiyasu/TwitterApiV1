d000fff0dfd439ed14f2e0a0ce94e66c8e96c741
daudailiyasu <51734692+daudailiyasu@users.noreply.github.com>
2020-11-01 18:40:52 -0800

    third static

--- TwitterApiProj/settings.py
+++ TwitterApiProj/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
-os.makedirs(STATIC_ROOT, exist_ok=True)
-STATICFILES_DIR=[
-    os.path.join(BASE_DIR, "static")
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
